fc0972592500a3bb108dc0f9c22d65a2e75b1a12
Bbuunn <bunnyryou@gmail.com>
2023-04-07 18:23:24 +1000

    Add hero

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -15,11 +15,26 @@ function HomePage() {
       })
   }, []) //comment: run when page first loads
   return (
-    <div>
-      {projectList.map((projectData, key) => {
-        return <ProjectCard key={key} data={projectData} />
-      })}
-    </div>
+    <>
+      <div className="hero bg-base-200 py-12">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Hello there</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+      <div className="p-8">
+        {projectList.map((projectData, key) => {
+          return <ProjectCard key={key} data={projectData} />
+        })}
+      </div>
+    </>
   )
 }
 
