7543d14678c26f9d0a340016f409ebbaf8164e12
Bbuunn <bunnyryou@gmail.com>
2023-04-10 20:15:47 +1000

    Style create acc page and add email as field

--- src/components/CreateAccountForm/CreateAccountForm.jsx
+++ src/components/CreateAccountForm/CreateAccountForm.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom"
 function CreateAccountForm() {
   const [formData, setFormData] = useState({
     username: "",
-    // email: "",
+    email: "",
     password: "",
   })
 
@@ -38,30 +38,62 @@ function CreateAccountForm() {
   }
 
   return (
-    <form>
-      <div>
-        <label htmlFor="username">Username:</label>
-        <input
-          onChange={handleChange}
-          type="text"
-          id="username"
-          placeholder="Enter username"
-        ></input>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content">
+        <div className="card w-full shadow-2xl bg-base-100">
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label" htmlFor="username">
+                <span className="label-text">Username</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Enter username"
+                className="input input-bordered"
+                id="username"
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="form-control">
+              <label className="label" htmlFor="email">
+                <span className="label-text">Email:</span>
+              </label>
+              <input
+                className="input input-bordered"
+                onChange={handleChange}
+                type="text"
+                id="email"
+                placeholder="Enter email"
+              />
+            </div>
+
+            <div className="form-control">
+              <label className="label" htmlFor="password">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                onChange={handleChange}
+                type="password"
+                id="password"
+                placeholder="Password"
+                className="input input-bordered"
+              />
+            </div>
+
+            <div className="form-control mt-6">
+              <button
+                className="btn btn-primary"
+                type="submit"
+                onClick={handleSubmit}
+              >
+                Create Account
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input
-          onChange={handleChange}
-          type="password"
-          id="password"
-          placeholder="Password"
-        ></input>
-      </div>
-      <button type="submit" onClick={handleSubmit}>
-        Login
-      </button>
-      {/* button within a function - need to specify type, will use for pledge */}
-    </form>
+    </div>
   )
 }
 
